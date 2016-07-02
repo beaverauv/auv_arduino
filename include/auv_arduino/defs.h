1579b879c54c1aaf203c0e235a48cb2f6335c2c1
Oliver <olivergeller120@gmail.com>
2016-07-02 12:40:31 -0400

    changed pin defs because rip second arduino

--- include/auv_arduino/defs.h
+++ include/auv_arduino/defs.h
@@ -17,13 +17,13 @@
 #define MAX_PWM 1900
 #define MIN_PWM 1100
 
-#define MOTOR_VFL_PIN 12
-#define MOTOR_VFR_PIN 11
-#define MOTOR_VBL_PIN 13
-#define MOTOR_VBR_PIN 10
-#define MOTOR_HFL_PIN 7
-#define MOTOR_HFR_PIN 8
-#define MOTOR_HBL_PIN 6
-#define MOTOR_HBR_PIN 9
+#define MOTOR_HFL_PIN 8
+#define MOTOR_HFR_PIN 9
+#define MOTOR_HBL_PIN 10
+#define MOTOR_HBR_PIN 11
+#define MOTOR_VFL_PIN 6
+#define MOTOR_VFR_PIN 5
+#define MOTOR_VBL_PIN 4
+#define MOTOR_VBR_PIN 3
 
 #endif
